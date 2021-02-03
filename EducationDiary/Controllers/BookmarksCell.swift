9c0af9996a2c876119f96379724b8fbd0370d0f7
Eugene St <=>
2021-02-03 16:16:23 +0200

    BookmarksVC: edit alertController on long tap added (not functional yet)

--- EducationDiary/Controllers/BookmarksCell.swift
+++ EducationDiary/Controllers/BookmarksCell.swift
@@ -8,10 +8,7 @@
 import UIKit
 
 class BookmarksCell: UITableViewCell {
-    
-    // todo:
-    // logic to copy text to clipboard / open in safari if link is valid
-    
+
     // long tap to edit
     
     override func awakeFromNib() {
@@ -21,22 +18,30 @@ class BookmarksCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
+        // copy text to clipboard or open in Safari if the link is valid
         if selected {
             if let text = self.detailTextLabel?.text {
                 if let url = URL(string: text) {
                     UIApplication.shared.open(url)
                 }
+                UIPasteboard.general.string = text
             }
         }
     }
-
+    
     func configure(with bookmarks: Bookmarks, indexPath: IndexPath) {
+        
         let bookmarkKeys = Array(bookmarks.keys)
         let bookmark = bookmarks[bookmarkKeys[indexPath.row]]
 
         self.textLabel?.text = bookmark?.name
         self.detailTextLabel?.text = bookmark?.text
         
-        UIPasteboard.general.string = bookmark?.text
+//        bookmarks.forEach { id, bookmark in
+//
+//            if bookmark.name == "" {
+//                self.detailTextLabel?.font = UIFont.systemFont(ofSize: 18)
+//            }
+//        }
     }
 }
--- EducationDiary/Controllers/BookmarksViewController.swift
+++ EducationDiary/Controllers/BookmarksViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class BookmarksViewController: UITableViewController {
-
+    
     // MARK: - Private Properties
     private var bookmarks = Bookmarks()
     
@@ -24,40 +24,40 @@ class BookmarksViewController: UITableViewController {
         NetworkManager.shared.getRequest(of: Bookmarks.self, path: "bookmarks.json") { result in
             switch result {
             case .failure(let error):
-                    if error is DataError {
-                        print(error)
-                    } else {
-                        print(error.localizedDescription)
-                    }
+                if error is DataError {
+                    print(error)
+                } else {
                     print(error.localizedDescription)
-                    
-                case .success(let bookmarks):
-                    DispatchQueue.main.async {
-                        self.bookmarks = bookmarks
-                        self.tableView.reloadData()
+                }
+                print(error.localizedDescription)
+                
+            case .success(let bookmarks):
+                DispatchQueue.main.async {
+                    self.bookmarks = bookmarks
+                    self.tableView.reloadData()
                 }
             }
         }
     }
-
+    
     // MARK: - Table view data source methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         bookmarks.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "bookmarkCell", for: indexPath) as! BookmarksCell
         
         cell.configure(with: bookmarks, indexPath: indexPath)
-
+        
         return cell
     }
-
+    
     // MARK: - Table View Delegate method
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete {
@@ -77,86 +77,117 @@ class BookmarksViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     // MARK: - IBActions
     @IBAction func addBookmarkPressed(_ sender: UIBarButtonItem) {
-        
-        var dataToPass: [String: String] = [:]
-        
-        let timeStamp = String(format: "%.0f", Date.timeIntervalSinceReferenceDate)
-        let id = "\(timeStamp)"
-        
-        let ac = UIAlertController(title: "Add bookmark", message: "Please enter Bookmark name and text", preferredStyle: .alert)
-        
-        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
-            
-            if let name = ac.textFields?.first?.text {
-                dataToPass["name"] = name
-            }
-            
-            if let text = ac.textFields?.last?.text {
-                dataToPass["text"] = text
-            }
-
-            NetworkManager.shared.putRequest(path: "/bookmarks/", id: id, body: dataToPass) { error in
-                if let err = error {
-                    print("Failed to put", err)
-                    return
-                }
-            }
-            
-            self.bookmarks[id] = Bookmark(name: dataToPass["name"], text: dataToPass["text"])
-            
-//            let insertionIndexPath = IndexPath(row: self.bookmarks.count - 1, section: 0)
-//            self.tableView.insertRows(at: [insertionIndexPath], with: .automatic)
-            
-            
-            self.tableView.reloadData()
-        }
-        
-        ac.addTextField { nameTextfield in
-            nameTextfield.placeholder = "name - optional"
-            nameTextfield.autocapitalizationType = .sentences
-        }
-        
-        ac.addTextField { textField in
-            textField.placeholder = "text - required"
-            textField.autocapitalizationType = .sentences
-
-            NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: OperationQueue.main) { _ in
-
-                let textCount = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines).count ?? 0
-
-                let textIsNotEmpty = textCount > 0
-
-                okAction.isEnabled = textIsNotEmpty
-            }
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
-
-        ac.addAction(cancelAction)
-        okAction.isEnabled = false
-        ac.addAction(okAction)
-        present(ac, animated: true)
+        callAlertController()
     }
     
     // MARK: - Private methods
     @objc private func longPressed(sender: UILongPressGestureRecognizer) {
-
+        
         if sender.state == UIGestureRecognizer.State.began {
-
+            
             let touchPoint = sender.location(in: self.tableView)
             if let indexPath = tableView.indexPathForRow(at: touchPoint) {
-
+                
                 let cell = tableView.cellForRow(at: indexPath)
-
+                
                 print("Long pressed row: \(cell?.detailTextLabel?.text ?? "No details")")
+                callAlertController(longTap: true)
             }
+        }
+    }
+    
+    private func callAlertController(longTap: Bool = false) {
+        if longTap == false {
+            
+            var dataToPass: [String: String] = [:]
+            
+            let timeStamp = String(format: "%.0f", Date.timeIntervalSinceReferenceDate)
+            let id = "\(timeStamp)"
+            
+            let ac = UIAlertController(title: "Add bookmark", message: "Please enter Bookmark name and text", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+                
+                if let name = ac.textFields?.first?.text {
+                    dataToPass["name"] = name
+                }
+                
+                if let text = ac.textFields?.last?.text {
+                    dataToPass["text"] = text
+                }
+                
+                NetworkManager.shared.putRequest(path: "/bookmarks/", id: id, body: dataToPass) { error in
+                    if let err = error {
+                        print("Failed to put", err)
+                        return
+                    }
+                }
+                
+                self.bookmarks[id] = Bookmark(name: dataToPass["name"], text: dataToPass["text"])
+                
+                //            let insertionIndexPath = IndexPath(row: self.bookmarks.count - 1, section: 0)
+                //            self.tableView.insertRows(at: [insertionIndexPath], with: .automatic)
+                
+                
+                self.tableView.reloadData()
+            }
+            
+            ac.addTextField { nameTextfield in
+                nameTextfield.placeholder = "name - optional"
+                nameTextfield.autocapitalizationType = .sentences
+            }
+            
+            ac.addTextField { textField in
+                textField.placeholder = "text - required"
+                textField.autocapitalizationType = .sentences
+                
+                NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: OperationQueue.main) { _ in
+                    
+                    let textCount = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines).count ?? 0
+                    
+                    let textIsNotEmpty = textCount > 0
+                    
+                    okAction.isEnabled = textIsNotEmpty
+                }
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+            
+            ac.addAction(cancelAction)
+            okAction.isEnabled = false
+            ac.addAction(okAction)
+            present(ac, animated: true)
+        } else {
+            
+            let ac = UIAlertController(title: "Edit bookmark", message: "You may edit the bookmark", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+                
+                print("Ok pressed")
+            }
+            
+            ac.addTextField { nameTextfield in
+                nameTextfield.placeholder = "name - optional"
+                nameTextfield.autocapitalizationType = .sentences
+            }
+            
+            ac.addTextField { textField in
+                textField.placeholder = "text - required"
+                textField.autocapitalizationType = .sentences
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+            
+            ac.addAction(cancelAction)
+            ac.addAction(okAction)
+            present(ac, animated: true)
         }
     }
 }
